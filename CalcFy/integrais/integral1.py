0a5904c9f2cebb53a8378dd71dd54896af3e3515
unknown <macieljr5587@gmail.com>
2023-06-27 20:27:52 -0400

    retiro de print e encrementação de retorno na função das integrais

--- CalcFy/integrais/integral1.py
+++ CalcFy/integrais/integral1.py
@@ -1,5 +1,5 @@
 import random
-from sympy import symbols, integrate
+from sympy import symbols, integrate, limit
 
 
 def positivoNegativo():
@@ -12,53 +12,68 @@ def positivoNegativo():
 def integrais1(quantidade,ativoResposta=False, ativoLimites=False):
     
     operacao = positivoNegativo()
-   
+
+    
+
+    listaFuncao = []
+    listaIntegral = []
+    listaLimiteA = []
+    listaLimiteB = []
+
+
     for _ in range(quantidade):
         
         x = symbols('x')
+
         a_coef = random.randint(-10, 10)
         b_coef = random.randint(-10, 10)
         c_coef = random.randint(-10, 10)
         d_coef = random.randint(-10, 10)
         e_coef = random.randint(-10, 10)
         f_coef = random.randint(-10, 10)
+
         expoente = random.randint(0, 6)
+
+        Limite_a = random.randint(0, 6)
+        Limite_b= random.randint(-5, 5)
+
         if operacao == 'positivo':
             functionU = a_coef * x**expoente + b_coef * x + c_coef
             functionV = f_coef * x**expoente + e_coef * x + f_coef
         else:
             functionU = a_coef * x**expoente + b_coef * x + c_coef
             functionV = f_coef * x**expoente + e_coef * x + f_coef
-        if ativoResposta == True:
-            if ativoLimites == True:
-                Limite_a = random.randint(0, 6)
-                Limite_b= random.randint(-5, 5)
-                integral = integrate(functionU, (x, Limite_a, Limite_b))*functionV + integrate(functionU, (x, Limite_a, Limite_b))*functionU
-                print("Função:({})x({})".format( functionU, functionV))
-                print("Integral:", integral)
-                print("Limite A:", Limite_a)
-                print("Limite B", Limite_b)
-                print()
 
-            else:
-                integral = integrate(functionU, (x))*functionV + integrate(functionU, (x))*functionU
-                print("Função:({})x({})".format( functionU, functionV))
-                print("Integral:", integral)
-                print()
+        integralU = integrate(functionU)
+        integralV = integrate(functionV)
+
+        function = functionU*functionV
+        integralResult = integralU * functionV + functionU * integralV
+
+        integralLimiteA = limit(integralResult, x, Limite_a)
+        integralLimiteB = limit(integralResult, x, Limite_b)
+
+        if ativoLimites: integralResult = integralLimiteA - integralLimiteB
+
+        listaFuncao.append(function)
+        listaIntegral.append(integralResult)
+        listaLimiteA.append(Limite_a)
+        listaLimiteB.append(Limite_b)
+
+
+    if ativoResposta:
+        if ativoLimites:
+            return listaFuncao, listaIntegral, listaLimiteA, listaLimiteB
         else:
-            if ativoLimites == True:
-                Limite_a = random.randint(-5, 6)
-                Limite_b= random.randint(-5, 6)
-                integral = integrate(functionU, (x, Limite_a, Limite_b)) 
-                print("Função:({})x({})".format( functionU, functionV))
-                print()
+            return listaFuncao, listaIntegral
+    else:
+        if ativoLimites:
+            return listaFuncao, listaLimiteA, listaLimitB
+        else:
+            return listaFuncao
 
-            else:
-                integral = integrate(functionU, (x))
-                print("Função:({})x({})".format( functionU, functionV))
-                print()
        
        
         
 
-integrais1(5, ativoResposta=True)
+print(integrais1(1, ativoResposta=True))
--- CalcFy/integrais/integralSimples.py
+++ CalcFy/integrais/integralSimples.py
@@ -11,49 +11,53 @@ def positivoNegativo():
 
 def integraisSimples(quantidade,ativoResposta=False, ativoLimites=False):
     
+    listaFuncao = []
+    listaIntegral = []
+    listaLimiteA = []
+    listaLimiteB = []
     operacao = positivoNegativo()
     # Gere cálculos de integrais aleatórias
     for _ in range(quantidade):
         # Gere uma função aleatória
+        
+        Limite_a = random.randint(-5, 6)
+        Limite_b= random.randint(-5, 6)
+
         x = symbols('x')
         a_coef = random.randint(-10, 10)
         b_coef = random.randint(-10, 10)
         c_coef = random.randint(-10, 10)
         expoente = random.randint(0, 6)
+
+
         if operacao == 'positivo':
             function = a_coef * x**2 + b_coef * x + c_coef
         else:
             function = a_coef * x**2 - b_coef * x + c_coef
-        if ativoResposta == True:
-            if ativoLimites == True:
-                Limite_a = random.randint(-5, 6)
-                Limite_b= random.randint(-5, 6)
-                integral = integrate(function, (x, Limite_a, Limite_b)) 
-                print("Função:", function)
-                print("Integral:", integral)
-                print("Limite A:", Limite_a)
-                print("Limite B", Limite_b)
-                print()
+        
 
-            else:
-                integral = integrate(function, (x))
-                print("Função:", function)
-                print("Integral:", integral)
-                print()
+        integrate(function, (x))
+
+
+        if ativoLimites: integral = integrate(function, (x, Limite_a, Limite_b))
+    
+        listaFuncao.append(function)
+        listaIntegral.append(integral)
+        listaLimiteA.append(Limite_a)
+        listaLimiteB.append(Limite_b)
+
+    if ativoResposta == True:
+        if ativoLimites == True:
+            return listaFuncao, listaIntegral, listaLimiteA, listaLimiteB
         else:
-            if ativoLimites == True:
-                Limite_a = random.randint(-5, 6)
-                Limite_b= random.randint(-5, 6)
-                integral = integrate(function, (x, Limite_a, Limite_b)) 
-                print("Função:", function)
-                print()
-
-            else:
-                integral = integrate(function, (x))
-                print("Função:", function)
-                print()
+            return listaFuncao, listaIntegral 
+    else:
+        if ativoLimites == True:
+            return listaFuncao,listaLimiteA, listaLimiteB
+        else:
+            return listaFuncao
        
        
         
 
-integraisSimples(5, ativoResposta=True, ativoLimites=True)
+print(integraisSimples(5, ativoResposta=True, ativoLimites=True))
